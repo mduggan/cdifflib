8f8c9b75f8379669ebdeffb34d2d8e5fd76fd330
Matthew Duggan <mgithub@guarana.org>
2013-12-09 17:26:06 +0900

    Switch to setuptools.

--- Makefile
+++ Makefile
@@ -4,11 +4,14 @@ build:
 install:
 	python setup.py install
 
+test:
+	python setup.py test
+
 clean:
 	python setup.py clean
 	rm -rf build/
+	rm -rf CDiffLib.egg-info
+	rm -f _cdifflib.so
 	rm -f *.pyc
 	rm -f tests/*.pyc
 
-test:
-	python -m unittest discover -v -s . -t . -p *_tests.py
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 module1 = Extension('_cdifflib',
                     sources=['_cdifflib.c'])
@@ -6,4 +6,20 @@ module1 = Extension('_cdifflib',
 setup(name='CDiffLib',
       version='1.0',
       description='C implementation of parts of difflib',
-      ext_modules=[module1], py_modules=['cdifflib'])
+      ext_modules=[module1],
+      py_modules=['cdifflib'],
+      test_suite='tests',
+
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: Proprietary',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Software Development',
+          'Topic :: Text Processing :: General',
+      ],
+      )
